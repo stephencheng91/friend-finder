3121504b40c308a12abdeaad9d8d46bd0ba63bd3
KuanToto <stephencheng91@gmail.com>
2019-05-28 20:43:08 -0700

    rev -

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,8 @@
 //var express = require("express");
 //var router = express.Router();
 
-var friendsData = require("../data/friends")
-console.log(friendsData)
+var friendsData = require("../data/friends");
+console.log(friendsData);
 
 module.exports = function (app) {
 
@@ -13,9 +13,21 @@ module.exports = function (app) {
     });
 
     app.post("/api/friends", function (req, res) {
-        var user = req.body;
-        friendsData.push(user);
+        //Getting current user
+        var newUserName = req.body.name;
 
+        //Loop to each user on the datalist
+        for (var i = 0; i < friendsData.length; i++) {
+            var user = friendsData[i];
+
+            var scoresDiff = 0;
+            for (var j = 0; j < user.scores.length; j++) {
+                //Getting the difference between each saved user to current user
+                scoresDiff += Math.abs(user.scores[j] - req.body.userValue[j]);
+            }
+            console.log("user: " + user.name + " Score Diff: " + scoresDiff);
+
+        }
     });
 
 }
